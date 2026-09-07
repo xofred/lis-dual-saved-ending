500d1ed9bd2b521ed85f6a81bce92d1e70441614
xofred <fwin3000@gmail.com>
2026-09-07 20:17:06 +0800

    asset: 新增故事和多媒體

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* 由 build.py 產生,請勿手動編輯 */
-const VERSION = 'vc2274fc80547';
+const VERSION = 'v001ad6df863f';
 const SHELL = 'shell-' + VERSION;
 const MEDIA = 'media-' + VERSION;
 const SHELL_MAX = 130;
